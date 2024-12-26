dc7e1252635f4224e936b59b67b66c3c71526498
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-26 19:04:45 +0000

    update

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-// Spinner
-var spinner = function () {
-    setTimeout(function () {
-        if ($('#spinner').length > 0) {
-            $('#spinner').removeClass('show');
-        }
-    }, 1);
-};
-spinner();
